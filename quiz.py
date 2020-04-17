7a0acf606da21625dba3f64bb5b459dc759ca8d5
Jan <jasiufilip@gmail.com>
2020-04-17 18:37:58 +0200

    requiredquestion

--- quiz.py
+++ quiz.py
@@ -10,7 +10,7 @@ respondent = Respondent()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('part1.html')
 
 @app.route('/czesc1', methods=['GET', 'POST'])
 def index2():
@@ -18,9 +18,9 @@ def index2():
         odebrane = request.form
         respondent.wpisz_dane(odebrane)
         if odebrane.get("2") == 'Nigdy nie zetknałem sie z tym pojeciem':
-            return render_template('index3.html')
+            return render_template('end1.html')
         else:
-            return render_template('index2.html')
+            return render_template('part2.html')
 
 
 @app.route('/czesc2', methods=['GET', 'POST'])
@@ -30,7 +30,7 @@ def index3():
         respondent.dopisz_dane(odebrane)
         respondent.wywietl_dane()
         respondent.zapis_do_bazy()
-        return render_template('index4.html')
+        return render_template('end2.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
