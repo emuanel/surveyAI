3ad190d9961ec0f6e7040fd0b98da742bdfd8be9
Jacek <j.rojek19@gmail.com>
2020-04-18 18:50:56 +0100

    refactor quiz

--- quiz.py
+++ quiz.py
@@ -1,36 +1,30 @@
-from flask import request
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template, session
 from Respondent import Respondent
 
 app = Flask(__name__)
+app.secret_key = "hello"
 
-respondent = Respondent()
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
     return render_template('part1.html')
 
-@app.route('/czesc1', methods=['GET', 'POST'])
-def index2():
-    if request.method == 'POST':
-        odebrane = request.form
-        respondent.wpisz_dane(odebrane)
-        if odebrane.get("2") == 'Nigdy nie zetknałem sie z tym pojeciem':
-            return render_template('end1.html')
-        else:
-            return render_template('part2.html')
+@app.route('/czesc1', methods=['POST'])
+def czesc1():
+    if request.form.get("2") == 'Nigdy nie zetknałem sie z tym pojeciem':
+        return render_template('end1.html')
+    else:
+        session['form1'] = request.form
+        return render_template('part2.html')
 
-
-@app.route('/czesc2', methods=['GET', 'POST'])
-def index3():
-    if request.method == 'POST':
-        odebrane = request.form
-        respondent.dopisz_dane(odebrane)
-        respondent.wywietl_dane()
-        respondent.zapis_do_bazy()
-        return render_template('end2.html')
+@app.route('/czesc2', methods=['POST'])
+def czesc2():
+    odebrane = request.form
+    respondent = Respondent()
+    respondent.wpisz_dane(session['form1'])
+    respondent.dopisz_dane(odebrane)
+    respondent.wywietl_dane()
+    respondent.zapis_do_bazy()
+    return render_template('end2.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
